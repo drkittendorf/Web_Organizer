993d06891a7a002020f64e20281f61ac86dee34d
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-08-22 19:50:02 -0500

    heroku up!

--- config/config.js
+++ config/config.js
@@ -5,25 +5,25 @@ require('dotenv').config()
 if (process.env.JAWSDB_URL) {
     // Database is JawsDB on Heroku
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    // Database is local
-    connection = mysql.createConnection({
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    })
+// } else {
+//     // Database is local
+//     connection = mysql.createConnection({
+//         host: 'localhost',
+//         port: 3306,
+//         user: 'root',
+//         password: process.env.DB_PASSWORD,
+//         database: process.env.DB_NAME
+//     })
 };
 
 module.exports = {
-    development: {
-      username: 'root',
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      host:     'localhost',
-      dialect:  'mysql'
-    },
+    // development: {
+    //   username: 'root',
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_NAME,
+    //   host:     'localhost',
+    //   dialect:  'mysql'
+    // },
     
     production: {
         "use_env_variable": "JAWSDB_URL",
