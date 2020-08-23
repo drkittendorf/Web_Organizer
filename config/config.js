c4068b3d2ad1e93fa745cc7098e0d340bd397584
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-08-22 19:39:41 -0500

    heroku up!

--- config/config.js
+++ config/config.js
@@ -17,13 +17,13 @@ if (process.env.JAWSDB_URL) {
 };
 
 module.exports = {
-    // development: {
-    //   username: 'root',
-    //   password: process.env.DB_PASSWORD,
-    //   database: process.env.DB_NAME,
-    //   host:     'localhost',
-    //   dialect:  'mysql'
-    // },
+    development: {
+      username: 'root',
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host:     'localhost',
+      dialect:  'mysql'
+    },
     
     production: {
         "use_env_variable": "JAWSDB_URL",
