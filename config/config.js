a034db7ea4f36b5858dd10c7d6ab9aaa22c9464b
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-08-22 19:17:40 -0500

    heroku up!

--- config/config.js
+++ config/config.js
@@ -1,22 +1,6 @@
 require('dotenv').config()
 const mysql = require('mysql');
 
-module.exports = {
-    development: {
-      username: 'root',
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      host:     'localhost',
-      dialect:  'mysql'
-    },
-    
-    production: {
-        "use_env_variable": "JAWSDB_URL",
-        "dialect":  "mysql"
-  }
-}
-
-
 const connection;
 if (process.env.JAWSDB_URL) {
     // Database is JawsDB on Heroku
@@ -32,7 +16,20 @@ if (process.env.JAWSDB_URL) {
     })
 };
 
-
+module.exports = {
+    development: {
+      username: 'root',
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host:     'localhost',
+      dialect:  'mysql'
+    },
+    
+    production: {
+        "use_env_variable": "JAWSDB_URL",
+        "dialect":  "mysql"
+  }
+}
 
 
 
